bd8f4fd2b15e9ac89407e67b579ba2ec2615f507
Soo <68042068+heonsooo@users.noreply.github.com>
2020-11-05 19:58:09 +0900

    Update 회차별_당첨번호_랭킹_(보너스제외).py

--- 회차별_당첨번호_랭킹_(보너스제외).py
+++ 회차별_당첨번호_랭킹_(보너스제외).py
@@ -30,8 +30,20 @@ def lotto(df):
     Normal_list_print = []
     for j in Normal_list:
         Normal_list_print.append(j)
- 
+    
     number_of_lotto = main_Under_10+ main_Under_20+ main_Under_30+ main_Under_40 + main_other
+    per_1 = round(len(main_Under_10)/len(number_of_lotto),3)
+    per_10 = round(len(main_Under_20)/len(number_of_lotto),3)
+    per_20 = round(len(main_Under_30)/len(number_of_lotto),3)
+    per_30 = round(len(main_Under_40)/len(number_of_lotto),3)
+    per_40 = round(len(main_other)/len(number_of_lotto),3)
+    print('\n 1 - 9 ', Counter(main_Under_10) ,''*5,len(main_Under_10),''*5,per_1*100)
+    print('\n 10 - 19 ', Counter(main_Under_20),''*5,len(main_Under_20),''*5,per_10*100)
+    print('\n 20 - 29 ', Counter(main_Under_30),''*5,len(main_Under_30),''*5,per_20*100)
+    print('\n 30 - 39 ', Counter(main_Under_40),''*5,len(main_Under_40),''*5,per_30*100)
+    print('\n 40 - 45 ', Counter(main_other),''*5,len(main_other),''*5,per_40*100)
+
+
     print('\n 총 횟수 ', Counter(number_of_lotto))
     return print('끝')
 
